8ec2687fd2dc167170823f3f481afcff29124aef
choidami5126 <dahyun5003@kakao.com>
2023-02-19 00:13:35 +0900

    스스로 풀이한 문제

--- codingtest_14.js
+++ codingtest_14.js
@@ -1,14 +1,14 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/70128
 
+// a, b 배열 안에 있는 같은 인덱스의 수를 곱해 그 값들을 더한 값을 반환하는 문제이다.
 function solution(a, b) {
     var answer = 0;
     for(i = 0; i < a.length; i++) {
-        a[i] * b[i]  answer
+        answer += a[i] * b[i]
+        // 초기화한 변수 answer에 a[i]*b[i] 값을 모두 더 해준다.
     }
-    console.log(answer)
     return answer;
 }
-console.log([1,2,3,4], [-3,-1,0,2])
 
 
 
