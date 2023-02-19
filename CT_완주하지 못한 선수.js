ffd8c8a9bf1a90736261341b1b9ac02d145be967
choidami5126 <dahyun5003@kakao.com>
2023-02-20 07:18:45 +0900

    반만 스스로 풀이한 문제

--- CT_완주하지 못한 선수.js
+++ CT_완주하지 못한 선수.js
@@ -1,40 +1,21 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/42576
 
-// function solution(participant, completion) {
-//     let fail_list = participant.reduce((other_name, name) => {
-//         if(!completion.includes(name)) {
-//             other_name.push(name);
-//             console.log(other_name)
-//         }
-//         return other_name;
-//     },[]);
-//     answer = fail_list[0];
-// }
-// console.log( solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
-
 function solution(participant, completion) {
-    const fail_list = participant.reduce((acc, name) => {
-      if (!completion.includes(name)) {
-        acc.push(name);
-      }
-      return acc;
-    }, []);
-  
-    return fail_list;
+    const participant_list = participant.sort()
+    // participant 배열을 사전순으로 정렬한다.
+    const completion_list = completion.sort()
+    // completion 배열을 사전순으로 정렬한다.
+    for(let i = 0; i < participant_list.length; i++) {
+      if(participant_list[i] !== completion_list[i])
+      return participant_list[i]
+      // 정렬한 participant 배열을 반복하면서 정렬한 completion_list 배열중
+      // 같은 index의 이름이 일치하지 않는 사람을 반환한다.
+    }
   }
-  
-  console.log(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])); // "leo"
+
+// 해당 문제에를 처음 보고 filter 와 includes 를 사용해 첫 배열에서 두번째 배열에 없는 이름을 반환하려 했지만
+// 동명이인이 존재하는 지점에서 막혀 중복 삭제를 위한 Set 등 여러 시도를 하며 고전하였는데,
+// 본인은 sort로 풀었다는 팀원의 말을 듣고 sort로 식을 짜보니 너무나 간단하게 풀리는 것이 아닌가...
+// 새로 배운 메서드를 활용하고자 하는 마음에 너무 꼬아서 생각한 문제인 것 같다.
 
 
-  // function solution(participant, completion) {
-//     let success_list = participant.sort();
-//     let fail_list = completion.sort();
-
-//     for(let i = 0; i < success_list.length; i++) {
-//         for(let j = 0; j < fail_list.length; j++) {
-            
-//         }
-//     }
-    
-// }
-// console.log( solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
